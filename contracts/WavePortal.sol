d33162f731c293202a2046a7be7fb8a0b60aa1ae
damienteo <ksteo89@gmail.com>
2022-09-01 10:04:21 +0800

    Add contract base

---
+++ contracts/WavePortal.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: UNLICENSED
+
+pragma solidity ^0.8.9;
+
+import "hardhat/console.sol";
+
+contract WavePortal {
+    constructor() {
+        console.log("I am a smart contract");
+    }
+}
