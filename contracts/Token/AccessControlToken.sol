4259720f2a3c4f2f13952e6fe61245f69f719fa2
damienteo <damien.teo.ks@gmail.com>
2022-10-03 23:05:53 +0800

    Add AccessControlToken contract

---
+++ contracts/Token/AccessControlToken.sol
@@ -0,0 +1,29 @@
+//SPDX-License-Identifier: UNLICENSED
+
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/access/AccessControl.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract AccessControlToken is ERC20, AccessControl {
+    bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
+    bytes32 public constant BURNER_ROLE = keccak256("BURNER_ROLE");
+
+    // constructor(address minter, address burner)
+    constructor() ERC20("AccessControlToken", "ACT") {
+        // _setupRole(MINTER_ROLE, minter);
+        // _setupRole(BURNER_ROLE, burner);
+        _setupRole(DEFAULT_ADMIN_ROLE, msg.sender);
+    }
+
+    //TODO: Wht does adminRole in roleData do?
+
+    function mint(address to, uint256 amount) public onlyRole(MINTER_ROLE) {
+        // require(hasRole(MINTER_ROLE, msg.sender), "Caller is not a minter");
+        _mint(to, amount);
+    }
+
+    function burn(address from, uint256 amount) public onlyRole(BURNER_ROLE) {
+        _burn(from, amount);
+    }
+}
