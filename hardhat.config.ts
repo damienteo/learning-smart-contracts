d0c6534d13909f2479a8a1831e5a2b43f3ed37e9
damienteo <damien.teo.ks@gmail.com>
2022-11-27 09:31:55 +0800

    Update hardhat config for deployment to polygon mumbai

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -4,8 +4,13 @@ import "@openzeppelin/hardhat-upgrades";
 
 require("dotenv").config();
 
-const { STAGING_QUICKNODE_KEY, INFURA_API, PRIVATE_KEY, ETHERSCAN_API } =
-  process.env;
+const {
+  STAGING_QUICKNODE_KEY,
+  INFURA_API,
+  PRIVATE_KEY,
+  ETHERSCAN_API,
+  POLYGONSCAN_API,
+} = process.env;
 
 const config: HardhatUserConfig = {
   solidity: "0.8.9",
@@ -15,9 +20,16 @@ const config: HardhatUserConfig = {
       url: STAGING_QUICKNODE_KEY,
       accounts: [PRIVATE_KEY || ""],
     },
+    mumbai: {
+      url: "https://matic-mumbai.chainstacklabs.com/",
+      accounts: [PRIVATE_KEY || ""],
+    },
   },
   etherscan: {
-    apiKey: ETHERSCAN_API,
+    apiKey: {
+      goerli: ETHERSCAN_API || "",
+      polygonMumbai: POLYGONSCAN_API || "",
+    },
   },
   paths: { tests: "tests" },
 };
