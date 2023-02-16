806859ffd38062acc23393b6d0025bc4c2745f3e
Damien Teo <ksteo89@gmail.com>
2023-02-16 10:08:17 +0800

    Update MultiSend draft

--- contracts/Utility/MultiSend.sol
+++ contracts/Utility/MultiSend.sol
@@ -1,5 +1,20 @@
+// Ref: https://medium.com/coinmonks/implement-multi-send-on-ethereum-by-smart-contract-with-solidity-47e0bf82b60c
+// Ref: https://github.com/encoderafat/disperse/blob/main/truffle/contracts/disperse.sol
+// Ref: https://github.com/banteg/disperse-research/blob/master/contracts/Disperse.sol
+// FIXME: This is just a very basic ideation draft
+
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.17;
+
+interface IERC20 {
+    function transfer(address to, uint256 value) external returns (bool);
+
+    function transferFrom(
+        address from,
+        address to,
+        uint256 value
+    ) external returns (bool);
+}
 
 contract MultiSend {
     address private owner;
@@ -47,17 +62,17 @@ contract MultiSend {
         return totalAmnt;
     }
 
-    function withdraw(address payable receiverAddr, uint256 receiverAmt)
-        private
-    {
+    function withdrawNetworkToken(
+        address payable receiverAddr,
+        uint256 receiverAmt
+    ) private {
         receiverAddr.transfer(receiverAmt);
     }
 
-    function withdrawals(address payable[] memory addrs, uint256[] memory amnts)
-        public
-        payable
-        isOwner
-    {
+    function disperseNetworkTokens(
+        address payable[] memory addrs,
+        uint256[] memory amnts
+    ) public payable isOwner {
         total_value += msg.value;
 
         require(
@@ -72,7 +87,48 @@ contract MultiSend {
         for (uint256 i; i < addrs.length; i++) {
             total_value -= amnts[i];
 
-            withdraw(addrs[i], amnts[i]);
+            withdrawNetworkToken(addrs[i], amnts[i]);
+        }
+    }
+
+    function disperseEther(
+        address payable[] calldata recipients,
+        uint256[] calldata values
+    ) external payable {
+        for (uint256 i; i < recipients.length; i++) {
+            recipients[i].transfer(values[i]);
+        }
+
+        uint256 balance = address(this).balance;
+        if (balance > 0) {
+            payable(msg.sender).transfer(balance);
+        }
+    }
+
+    function disperseToken(
+        IERC20 token,
+        address[] calldata recipients,
+        uint256[] calldata values
+    ) external {
+        uint256 total = 0;
+        for (uint256 i; i < recipients.length; i++) {
+            total += values[i];
+        }
+
+        require(token.transferFrom(msg.sender, address(this), total));
+
+        for (uint256 i; i < recipients.length; i++) {
+            require(token.transfer(recipients[i], values[i]));
+        }
+    }
+
+    function disperseTokenSimple(
+        IERC20 token,
+        address[] calldata recipients,
+        uint256[] calldata values
+    ) external {
+        for (uint256 i; i < recipients.length; i++) {
+            require(token.transferFrom(msg.sender, recipients[i], values[i]));
         }
     }
 }
