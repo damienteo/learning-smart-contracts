b9af665a39ed3639291751d57a0912a92619fa19
Damien Teo <ksteo89@gmail.com>
2023-06-20 11:32:54 +0800

    Add helper method to verify contract during deploy

---
+++ scripts/verify-contract.ts
@@ -0,0 +1,30 @@
+import { run } from "hardhat";
+
+const network: Record<number, string> = {
+  80001: "polygon_mumbai",
+  137: "polygon",
+};
+
+const verify = async (
+  contractAddress: string,
+  args: any[],
+  chainId?: number
+) => {
+  console.log("Verifying contract...");
+
+  if (!chainId || !network[chainId]) {
+    console.log("Invalid ChainId");
+    return;
+  }
+
+  try {
+    await run("verify:verify", {
+      address: contractAddress,
+      constructorArguments: args,
+      network: network[chainId],
+    });
+  } catch (e) {
+    console.log(e);
+  }
+};
+export default verify;
