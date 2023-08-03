40ec6658e92299202412c8cb4ec7dbcc7287ddc6
damienteo <damien.teo.ks@gmail.com>
2023-08-03 08:02:12 +0800

    Add UniswapV3SwapExample

--- contracts/Defi/UniswapV3SwapExample.sol
+++ contracts/Defi/UniswapV3SwapExample.sol
@@ -12,6 +12,7 @@ contract UniswapV3SwapExample {
 
     address public constant DAI = 0x6B175474E89094C44Da98b954EedeAC495271d0F;
     address public constant WETH9 = 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2;
+    address public constant USDC = 0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48;
     uint24 public constant feeTier = 3000;
 
     constructor(ISwapRouter _swapRouter) {
@@ -19,6 +20,7 @@ contract UniswapV3SwapExample {
     }
 
     /* 
+        Swaps exact number of tokens for tokens
         Should convert to a more generalised method
         Additional params:
          - address token0 
@@ -56,6 +58,9 @@ contract UniswapV3SwapExample {
         return amountOut;
     }
 
+    /* 
+        Swaps tokens for exact output
+    */
     function swapDaiForExactWeth(
         uint256 amountOut,
         uint256 amountInMaximum
@@ -87,6 +92,73 @@ contract UniswapV3SwapExample {
             TransferHelper.safeApprove(DAI, address(swapRouter), 0);
             TransferHelper.safeTransfer(
                 DAI,
+                msg.sender,
+                amountInMaximum - amountIn
+            );
+        }
+    }
+
+    /*
+        Multihop
+    */
+    function swapExactDaiToUSDCToWethMultihop(
+        uint256 amountIn
+    ) external returns (uint256 amountOut) {
+        TransferHelper.safeTransferFrom(
+            DAI,
+            msg.sender,
+            address(this),
+            amountIn
+        );
+
+        TransferHelper.safeApprove(DAI, address(swapRouter), amountIn);
+
+        ISwapRouter.ExactInputParams memory params = ISwapRouter
+            .ExactInputParams({
+                path: abi.encodePacked(DAI, feeTier, USDC, feeTier, WETH9),
+                recipient: msg.sender,
+                deadline: block.timestamp,
+                amountIn: amountIn,
+                amountOutMinimum: 0
+            });
+
+        // Executes the swap.
+        amountOut = swapRouter.exactInput(params);
+    }
+
+    function swapExactOutputMultihop(
+        uint256 amountOut,
+        uint256 amountInMaximum
+    ) external returns (uint256 amountIn) {
+        TransferHelper.safeTransferFrom(
+            DAI,
+            msg.sender,
+            address(this),
+            amountInMaximum
+        );
+
+        TransferHelper.safeApprove(DAI, address(swapRouter), amountInMaximum);
+
+        // The parameter path is encoded as (tokenOut, fee, tokenIn/tokenOut, fee, tokenIn)
+        // The tokenIn/tokenOut field is the shared token between the two pools used in the multiple pool swap. In this case USDC is the "shared" token.
+        // For an exactOutput swap, the first swap that occurs is the swap which returns the eventual desired token.
+        // In this case, our desired output token is WETH9 so that swap happens first, and is encoded in the path accordingly.
+        ISwapRouter.ExactOutputParams memory params = ISwapRouter
+            .ExactOutputParams({
+                path: abi.encodePacked(WETH9, feeTier, USDC, feeTier, DAI),
+                recipient: msg.sender,
+                deadline: block.timestamp,
+                amountOut: amountOut,
+                amountInMaximum: amountInMaximum
+            });
+
+        amountIn = swapRouter.exactOutput(params);
+
+        if (amountIn < amountInMaximum) {
+            TransferHelper.safeApprove(DAI, address(swapRouter), 0);
+            TransferHelper.safeTransferFrom(
+                DAI,
+                address(this),
                 msg.sender,
                 amountInMaximum - amountIn
             );
