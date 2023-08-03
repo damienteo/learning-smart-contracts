d35c133680e71c106c5498ae9884b855c09cb729
damienteo <damien.teo.ks@gmail.com>
2023-08-03 08:56:04 +0800

    Add UniswapV3PairFlashExample

--- contracts/Defi/UniswapV3LiquidityExample.sol
+++ contracts/Defi/UniswapV3LiquidityExample.sol
@@ -87,6 +87,10 @@ contract UniswapV3LiquidityExample is IERC721Receiver {
         uint256 amount0ToMint = 1000;
         uint256 amount1ToMint = 1000;
 
+        // Transfering not in Uniswap example?
+        TransferHelper.safeTransfer(DAI, address(this), amount0ToMint);
+        TransferHelper.safeTransfer(USDC, address(this), amount1ToMint);
+
         // Approve the position manager
         TransferHelper.safeApprove(
             DAI,
@@ -178,5 +182,69 @@ contract UniswapV3LiquidityExample is IERC721Receiver {
         // send collected fees to owner
         TransferHelper.safeTransfer(token0, owner, amount0);
         TransferHelper.safeTransfer(token1, owner, amount1);
+    }
+
+    function decreaseLiquidityInHalf(
+        uint256 tokenId
+    ) external returns (uint256 amount0, uint256 amount1) {
+        // caller must be the owner of the NFT
+        require(msg.sender == deposits[tokenId].owner, "Not the owner");
+        // get liquidity data for tokenId
+        uint128 liquidity = deposits[tokenId].liquidity;
+        uint128 halfLiquidity = liquidity / 2;
+
+        // amount0Min and amount1Min are price slippage checks
+        // if the amount received after burning is not greater than these minimums, transaction will fail
+        INonfungiblePositionManager.DecreaseLiquidityParams
+            memory params = INonfungiblePositionManager
+                .DecreaseLiquidityParams({
+                    tokenId: tokenId,
+                    liquidity: halfLiquidity,
+                    amount0Min: 0,
+                    amount1Min: 0,
+                    deadline: block.timestamp
+                });
+
+        (amount0, amount1) = nonfungiblePositionManager.decreaseLiquidity(
+            params
+        );
+
+        //send liquidity back to owner
+        _sendToOwner(tokenId, amount0, amount1);
+    }
+
+    function _sendToOwner(
+        uint256 tokenId,
+        uint256 amount0,
+        uint256 amount1
+    ) internal {
+        // get owner of contract
+        address owner = deposits[tokenId].owner;
+
+        address token0 = deposits[tokenId].token0;
+        address token1 = deposits[tokenId].token1;
+        // send collected fees to owner
+        TransferHelper.safeTransfer(token0, owner, amount0);
+        TransferHelper.safeTransfer(token1, owner, amount1);
+    }
+
+    function increaseLiquidityCurrentRange(
+        uint256 tokenId,
+        uint256 amountAdd0,
+        uint256 amountAdd1
+    ) external returns (uint128 liquidity, uint256 amount0, uint256 amount1) {
+        INonfungiblePositionManager.IncreaseLiquidityParams
+            memory params = INonfungiblePositionManager
+                .IncreaseLiquidityParams({
+                    tokenId: tokenId,
+                    amount0Desired: amountAdd0,
+                    amount1Desired: amountAdd1,
+                    amount0Min: 0,
+                    amount1Min: 0,
+                    deadline: block.timestamp
+                });
+
+        (liquidity, amount0, amount1) = nonfungiblePositionManager
+            .increaseLiquidity(params);
     }
 }
